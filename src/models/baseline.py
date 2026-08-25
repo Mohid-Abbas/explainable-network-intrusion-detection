f7c24ffa6e4e5bf72a320cb3f4bd02a0175a781d
Mohid-Abbas <mohidabbas1001@gmail.com>
2026-08-25 14:44:43 +0500

    created baseline of the model

---
+++ src/models/baseline.py
@@ -0,0 +1,11 @@
+import joblib
+import numpy as np
+import pandas as pd
+from sklearn.linear_model import LogisticRegression
+from src.config import MODEL_PARAMS
+
+
+def train_logistic_regression(X_train: np.ndarray, y_train: pd.Series) -> LogisticRegression:
+    model = LogisticRegression(max_iter=1000, class_weight="balanced", random_state=42, n_jobs=-1)
+    model.fit(X_train, y_train)
+    return model
